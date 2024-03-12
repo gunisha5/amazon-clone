685f530fcc736535306db0b4f81c2d9d7add7871
gunisha <gunisha@gmail.com>
2024-03-12 17:03:19 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className='home'>
+    <div className='home_container'>
+        <img
+        src=''
+        />
+    </div>
+    </div>
+  )
+}
+
+export default Home
